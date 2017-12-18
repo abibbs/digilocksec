c5d836cbe221483d998c1cc66aac4498f9574816
Anthojny Bibbs <anthonybibbs@gmail.com>
2017-12-18 14:38:21 -0800

    Linted routes/index.js for spacing

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const update = require('../auth/update');
 
 function isAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) { return next(); }
-	// if the user is not authenticated then redirect them to the login page
-	res.redirect('/');
+  if (req.isAuthenticated()) { return next(); }
+  // if the user is not authenticated then redirect them to the login page
+  res.redirect('/');
 }
 
 module.exports = function(passport) {
@@ -42,62 +42,62 @@ module.exports = function(passport) {
   });
 
   /* POST sign-up */
-  	router.post('/sign-up', passport.authenticate('signup', {
-      failureMessage: "User already exists",
-  		failureRedirect: '/sign-up',
-  		failureFlash : true
-  	}),function(req, res) {
-      console.log('registration successful...rerouting to confirm');
-      res.send({redirect: '/home'});
-    });
+  router.post('/sign-up', passport.authenticate('signup', {
+    failureMessage: "User already exists",
+    failureRedirect: '/sign-up',
+    failureFlash : true
+  }),function(req, res) {
+    console.log('registration successful...rerouting to confirm');
+    res.send({redirect: '/home'});
+  });
 
-    /* POST login */
-  	router.post('/login', passport.authenticate('login', {
-      failureMessage: "Login failed.",
-  		failureRedirect: '/sign-up',
-  		failureFlash : true
-  	}),function(req, res) {
-      console.log('login successful...rerouting to confirm');
-      res.send({redirect: '/home'});
-    });
+  /* POST login */
+  router.post('/login', passport.authenticate('login', {
+    failureMessage: "Login failed.",
+    failureRedirect: '/sign-up',
+    failureFlash : true
+  }),function(req, res) {
+    console.log('login successful...rerouting to confirm');
+    res.send({redirect: '/home'});
+  });
 
-    /* POST update settings */
-    router.post('/update-settings', function(req,res) {
-      update(req,res);
-    });
+  /* POST update settings */
+  router.post('/update-settings', function(req,res) {
+    update(req,res);
+  });
 
-    /* GET logout */
-  	router.get('/signout', function(req, res) {
-  		req.logout();
-  		res.redirect('/');
-  	});
+  /* GET logout */
+  router.get('/signout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
   /* GET user session data */
-    router.get('/api/user_data', function(req, res) {
-      if (req.user === undefined) {
-        // The user is not logged in
-        res.json({});
-      } else {
-        res.json({
-          user: req.user
-        });
-      }
-    });
-
-    /* GET user errors */
-      router.get('/api/user_errors', function(req, res) {
-        console.log('/api/user_errors',req.session.messages);
-
-        if (req.session.messages === undefined || !req.session.messages.length) {
-          // No errors
-          res.json({});
-        } else {
-          res.json({
-            errors: req.session.messages
-          });
-        }
-        req.session.messages = [];
+  router.get('/api/user_data', function(req, res) {
+    if (req.user === undefined) {
+      // The user is not logged in
+      res.json({});
+    } else {
+      res.json({
+        user: req.user
       });
+    }
+  });
+
+  /* GET user errors */
+  router.get('/api/user_errors', function(req, res) {
+    console.log('/api/user_errors',req.session.messages);
+
+    if (req.session.messages === undefined || !req.session.messages.length) {
+      // No errors
+      res.json({});
+    } else {
+      res.json({
+        errors: req.session.messages
+      });
+    }
+    req.session.messages = [];
+  });
 
   return router;
 };
