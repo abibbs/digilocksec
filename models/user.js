e51665daaaa86df74a6efb6a3cde46c2a2c0d3e3
Anthojny Bibbs <anthonybibbs@gmail.com>
2017-12-18 14:35:30 -0800

    Linted models/user.js for spacing

--- models/user.js
+++ models/user.js
@@ -6,8 +6,8 @@ const modelName = "user";
 mongoose.connect(db.uri, { useMongoClient: true });
 
 var userSchema = new mongoose.Schema({
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model(modelName, userSchema, collectionName);
